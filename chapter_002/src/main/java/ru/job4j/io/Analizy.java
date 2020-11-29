ebe26e2c7407c8c567bcf8dd2d77e54191e1c120
Anton Starichenkov <anton.starichenkov@gmail.com>
2020-11-30 00:37:01 +0300

    2. Анализ доступности сервера.[859#409724] -3

--- chapter_002/src/main/java/ru/job4j/io/Analizy.java
+++ chapter_002/src/main/java/ru/job4j/io/Analizy.java
@@ -8,30 +8,32 @@ import java.util.*;
 
 public class Analizy {
     public void unavailable(String source, String target) {
-        List<List<String>> sourceList = new ArrayList<>();
+        List<String> result = new ArrayList<>();
+        String unavailable = null;
+        String available = null;
+        String[] current;
 
         try (BufferedReader read = new BufferedReader(new FileReader(source))) {
             while (read.ready()) {
                 String s = read.readLine();
-                String[] strings = s.split(" ");
-                sourceList.add(Arrays.asList(strings));
+                current = s.split(" ");
+                if ((current[0].equals("500") || current[0].equals("400")) && unavailable == null) {
+                    unavailable = current[1];
+                    available = null;
+                }
+                if ((current[0].equals("200") || current[0].equals("300")) && unavailable != null) {
+                    available = current[1];
+                }
+                if (unavailable != null && available != null) {
+                    result.add(String.format("%s;%s", unavailable, available));
+                    unavailable = null;
+                    available = null;
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        List<String> prevEl = sourceList.get(0);
-        List<String> result = new ArrayList<>();
-        for (int i = 1; i < sourceList.size(); i++) {
-            List targetEl = sourceList.get(i);
-            if ((prevEl.get(0).equals("400") || prevEl.get(0).equals("500"))
-                    && (targetEl.get(0).equals("200") || targetEl.get(0).equals("300"))) {
-                result.add(prevEl.get(1) + ";" + targetEl.get(1));
-                prevEl = sourceList.get(i);
-            }
-            if (prevEl.get(0).equals("200") || prevEl.get(0).equals("300")) {
-                prevEl = sourceList.get(i);
-            }
-        }
+
         try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
             result.forEach(out::println);
         } catch (Exception e) {
