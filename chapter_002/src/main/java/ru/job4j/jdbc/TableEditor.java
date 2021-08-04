fa3f63d8a3963a85e627195eed55526180e0070a
Anton Starichenkov <anton.starichenkov@gmail.com>
2021-08-05 00:43:48 +0300

    0.1. Statement [#379306]

---
+++ chapter_002/src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -0,0 +1,127 @@
+package ru.job4j.jdbc;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+import java.util.StringJoiner;
+
+public class TableEditor implements AutoCloseable {
+
+    private Connection connection;
+
+    private Properties properties;
+
+    public TableEditor(Properties properties) throws ClassNotFoundException, SQLException {
+        this.properties = properties;
+        try {
+            properties.load(new FileInputStream("app.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        initConnection();
+    }
+
+    private void initConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("org.postgresql.Driver");
+        String url = properties.getProperty("url");
+        String login = properties.getProperty("login");
+        String password = properties.getProperty("password");
+        connection = DriverManager.getConnection(url, login, password);
+    }
+
+    public void createTable(String tableName) {
+        String queryStr = String.format(
+                "create table if not exists %s(%s, %s);",
+                tableName,
+                "id serial primary key",
+                "name varchar(255)"
+        );
+        executeQuery(queryStr);
+    }
+
+    public void dropTable(String tableName) {
+        String queryStr = String.format(
+                "drop table if exists %s;",
+                tableName
+        );
+        executeQuery(queryStr);
+    }
+
+    public void addColumn(String tableName, String columnName, String type) {
+        String queryStr = String.format(
+                "alter table %s add column %s %s",
+                tableName, columnName, type
+        );
+        executeQuery(queryStr);
+    }
+
+    public void dropColumn(String tableName, String columnName) {
+        String queryStr = String.format(
+                "alter table %s drop column %s",
+                tableName, columnName
+        );
+        executeQuery(queryStr);
+    }
+
+    public void renameColumn(String tableName, String columnName, String newColumnName) {
+        String queryStr = String.format(
+                "alter table %s rename column %s to %s",
+                tableName, columnName, newColumnName
+        );
+        executeQuery(queryStr);
+    }
+
+    private void executeQuery(String queryStr) {
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(queryStr);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+
+    public static String getTableScheme(Connection connection, String tableName) throws Exception {
+        var rowSeparator = "-".repeat(30).concat(System.lineSeparator());
+        var header = String.format("%-15s|%-15s%n", "NAME", "TYPE");
+        var buffer = new StringJoiner(rowSeparator, rowSeparator, rowSeparator);
+        buffer.add(header);
+        try (var statement = connection.createStatement()) {
+            var selection = statement.executeQuery(String.format(
+                    "select * from %s limit 1", tableName
+            ));
+            var metaData = selection.getMetaData();
+            for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                buffer.add(String.format("%-15s|%-15s%n",
+                        metaData.getColumnName(i), metaData.getColumnTypeName(i))
+                );
+            }
+        }
+        return buffer.toString();
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileInputStream("app.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String demoTable = "demo_table";
+        TableEditor tableEditor = new TableEditor(properties);
+        tableEditor.dropTable(demoTable);
+        tableEditor.createTable(demoTable);
+        tableEditor.addColumn(demoTable, "role", "varchar(255)");
+        tableEditor.dropColumn(demoTable, "name");
+        tableEditor.renameColumn(demoTable, "role", "access_level");
+        System.out.println(getTableScheme(tableEditor.connection, demoTable));
+
+    }
+}
