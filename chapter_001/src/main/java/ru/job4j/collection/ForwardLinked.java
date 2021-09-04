b785a0d48ee48448c8a9ba7b29d64d82ce03f0cd
Anton.Starichenkov <anton.starichenkov@gmail.com>
2021-09-04 08:42:28 +0300

    5. Очередь на двух стеках [#160 #198301]

--- chapter_001/src/main/java/ru/job4j/collection/ForwardLinked.java
+++ chapter_001/src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -21,9 +21,7 @@ public class ForwardLinked<T> implements Iterable<T> {
 
     public void addFirst(T value) {
         Node<T> node = new Node<T>(value, head);
-        if (head == null) {
             head = node;
-        }
     }
 
     @Override
--- chapter_001/src/main/java/ru/job4j/collection/SimpleQueue.java
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -1,28 +1,25 @@
 package ru.job4j.collection;
 
-import java.util.NoSuchElementException;
-
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
+    private int outSize;
+    private int inSize;
 
     public T poll() {
-        T value;
-        try {
-            value = out.pop();
-        } catch (NoSuchElementException e) {
-            try {
-                while (true) {
-                    out.push(in.pop());
-                }
-            } catch (NoSuchElementException ee) {
-                value = out.pop();
+        if (outSize == 0) {
+            while (inSize != 0) {
+                out.push(in.pop());
+                outSize++;
+                inSize--;
             }
         }
-        return value;
+        outSize--;
+        return out.pop();
     }
 
     public void push(T value) {
         in.push(value);
+        inSize++;
     }
 }
