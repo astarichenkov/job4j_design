65cb61e1a62738ee9890adec60e6bbc9b4fc0153
Anton.Starichenkov <anton.starichenkov@gmail.com>
2021-09-07 00:50:30 +0300

    5. Переопределить и equals и hashCode [#1002 #199840]

--- chapter_001/src/main/java/ru/job4j/map/User.java
+++ chapter_001/src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,6 @@
 package ru.job4j.map;
 
-import java.util.Calendar;
+import java.util.*;
 
 public class User {
     private String name;
@@ -11,5 +11,32 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+
+    public static void main(String[] args) {
+        Map<User, Object> userMap = new HashMap<>();
+        Calendar birthday1 = new GregorianCalendar(2017, Calendar.AUGUST , 25);
+        Calendar birthday2 = new GregorianCalendar(2017, Calendar.AUGUST , 25);
+        User one = new User("One", 1, birthday1);
+        User two = new User("One", 1, birthday2);
+        System.out.println(one.hashCode());
+        System.out.println(two.hashCode());
+        System.out.println(one.equals(two));
+        userMap.put(one, new Object());
+        userMap.put(two, new Object());
+        System.out.println(userMap);
     }
 }
